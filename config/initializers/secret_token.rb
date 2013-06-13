16bbfef06769bfad197231c46ac965a0844d981c
Ken Odegard <kho1@rice.edu>
2013-06-13 16:54:52 -0500

    Initial framework for widget

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oscpw::Application.config.secret_token = '314c79c699d1220b22d924dcac051c7412a5a36edf3256dbbdc15275fbadf3d086e422f1d98067e81c5bcbee953422ab3f7934dc760515b4bbc8af02e74d687b'
